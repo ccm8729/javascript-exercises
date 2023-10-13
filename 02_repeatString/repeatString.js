94f4041d5a016b9f23b753ca8c1af6166936ffa1
Chris Fedosick <cfedosick@gmail.com>
2023-10-13 17:27:59 -0400

    final project for js exercises 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,37 @@
-const repeatString = function() {
+const repeatString = function(word, times) 
+{
+let string =""
+if( times < 0)
+{
+    string = 'ERROR'
+    return string;
+    
+}
+else{
 
+for (let i = 0; i < times; i++)
+{
+
+    string = word+string;
+}
+
+return string;
+
+}
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+/*
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+  module.exports = repeatString;*/
+
