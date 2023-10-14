c6b300ba882d3a30ff8ea6f65e1b2d53586e05c3
Chris Fedosick <cfedosick@gmail.com>
2023-10-13 21:18:07 -0400

    finished project odin js 02

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let forward = string; 
+let len = string.length
+
+const reverse = Array.from(forward).reverse().join("");
+
+return reverse;
+
 
 };
 
