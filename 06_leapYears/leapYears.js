fc6e6941415635292eecb9282482065d7bc9ac0a
Chris Fedosick <cfedosick@gmail.com>
2023-10-14 17:06:16 -0400

    final commit for odin js project 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,43 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+//let year = 1996;
+let testYear = year;
+let four = testYear / 4;
+let fourHund = testYear / 400;
+let hundred = testYear / 100;
+
+console.log("year: " + year); 
+console.log("test Year: " + testYear);
+console.log(four);
+
+
+let fourCheck = Number.isInteger(four);
+let fourHundCheck = Number.isInteger(fourHund);
+let hundredCheck = Number.isInteger(hundred);
+
+console.log("fourCheck = " + four);
+console.log(fourCheck);
+
+
+if (hundredCheck === true && !fourHundCheck === true)
+{
+    return false;
+}
+else if (fourCheck === true || fourHundCheck === true)
+{
+    return true;
+}
+else
+{
+    return false;
+}
 
 };
 
+//leapYears();
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//&& fourHundCheck === "true" && hundredCheck === "true"
