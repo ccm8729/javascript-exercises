80036e0bb6b4e1bc0d6f2c054d3324b9e23be5ab
Chris Fedosick <cfedosick@gmail.com>
2023-10-13 22:24:21 -0400

    final commit of odin js project 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) 
+{
+    const start = [];
 
-};
+    array.forEach((item) =>
+    {
+        if(!args.includes(item))
+        {
+        start.push(item);
+        }
+    })
+    
+    return start;
+    };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
