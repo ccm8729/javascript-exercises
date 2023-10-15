e3a543ddd6f2c2c02258753c379a03c6ae88ca3e
Chris Fedosick <cfedosick@gmail.com>
2023-10-14 20:17:40 -0400

    final project submission odin js 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(convertToCelsius) {
+let cel = (5/9)*(convertToCelsius-32);
+cel = parseFloat(cel).toFixed(1);
+cel = Number(cel);
+return cel;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(convertToFahrenheit) {
+let far = convertToFahrenheit * (9/5) + 32;
+far = parseFloat(far).toFixed(1);
+far = Number(far);
+return far;
+
 };
 
 // Do not edit below this line
